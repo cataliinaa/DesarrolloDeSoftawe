eb9c3d891546fa88a78a029011d94ea3959e6d19
cataliinaa <62269057+cataliinaa@users.noreply.github.com>
2020-11-13 12:34:20 -0300

    version2

--- ev2V4/settings.py
+++ ev2V4/settings.py
@@ -130,3 +130,11 @@ STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static',
 LOGIN_REDIRECT_URL = 'homeUsuario'
 
 LOGOUT_REDIRECT_URL = 'homeUsuario'
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'django2020duoc@gmail.com'
+EMAIL_HOST_PASSWORD = '123duoc123'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- ev2V4/urls.py
+++ ev2V4/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from django.contrib.auth.views import LoginView , LogoutView
+from django.urls import path,include,reverse_lazy
+from django.contrib.auth.views import LoginView , LogoutView 
 from django.contrib.auth import views as auth_views
-
+from django.conf.urls import url
 from django.views.generic import TemplateView
 
 
@@ -26,16 +26,27 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('Gatos/',include('apps.Gatos.urls')),
     path('Registrar/',include('apps.Usuario.urls')),
-    
-    
     path('login/', auth_views.LoginView.as_view(template_name='Usuario/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='Usuario/logout.html'), name='logout'),
-    
-    
     path('accounts/login/', LoginView.as_view(redirect_authenticated_user=True,template_name='Usuario/login_superuser.html'), name='login_superuser'),
-
     path('', TemplateView.as_view(template_name='homeUsuario.html'), name='homeUsuario'),
+    path('accounts/', include('django.contrib.auth.urls')),
 
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='Password_reset/password_change_done.html'), 
+        name='password_change_done'),
 
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='Password_reset/password_change.html'), 
+        name='password_change'),
 
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='Password_reset/password_reset_done.html'),
+     name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='Password_reset/password_reset_complete.html'),
+     name='password_reset_complete'),
+    
+    
+    
 ]
